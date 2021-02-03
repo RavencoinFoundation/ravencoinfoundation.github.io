71da7a7a545146b7fe724955e87dc19c620236ef
Tron <tron@Trons-MacBook-Pro.local>
2021-02-02 18:29:01 -0700

    Add proposals

---
+++ Proposals/README.md
@@ -0,0 +1,6 @@
+# Proposals
+
+Add proposals with a filename format of
+[Display name].[IPFS content identifier].txt
+
+You can add a description in the file that may be used in the future, but currently unused.
---
+++ src/pages/proposals.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Container } from "../components/common/Container";
+import Layout from "../components/common/Layout";
+import getFileExtension from "../utils/getFileExtension";
+import theme from "../utils/theme";
+
+const StyledProposals = styled.div`
+  min-height: 50vh;
+  @media (min-width: ${theme.screens.tablet}) {
+  }
+  @media (min-width: ${theme.screens.laptop}) {
+  }
+`;
+
+function Proposals() {
+  const [minutesData, setMinutesData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        "https://api.github.com/repos/RavencoinFoundation/ravencoinfoundation.github.io/contents/Proposals"
+      );
+      const responseData = await response.json();
+      setIsLoading(false);
+
+      const cleanedMinutesData = responseData.filter(
+        (file) => getFileExtension(file.name) === "txt"
+      );
+
+      setMinutesData(cleanedMinutesData);
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <Layout>
+      <StyledProposals>
+        <Container>
+          <h2>Proposals</h2>
+          {isLoading ? (
+            <p>Loading...</p>
+          ) : (
+            <ul>
+              {minutesData.map((file) => (
+                <li key={file.sha}>
+                  <a href=gateway.ravencoinipfs.com/ipfs/{file.download_url}>{file.name}</a>
+                </li>
+              ))}
+            </ul>
+          )}
+        </Container>
+      </StyledProposals>
+    </Layout>
+  );
+}
+
+export default Proposals;
