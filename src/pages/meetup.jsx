966dc71edfc1d5c77c64ae17bddd4a4890ed56b0
Tron <tron@Trons-MacBook-Pro.local>
2021-06-09 14:30:30 -0600

    Add meetup (again)

--- src/pages/meetup.jsx
+++ src/pages/meetup.jsx
@@ -29,23 +29,30 @@ function Meetup() {
           <h2>
             Ravencoin Meetup 2021
           </h2>
+          
             <p>
-            I don't know <strong>WHY</strong> Ravencoin attracts the best people in the world.  I only know that it <strong>DOES</strong>.</p>
-            <p>
+            I don't know <strong>WHY</strong> Ravencoin attracts the best people in the world.  I only know that it <strong>DOES</strong>.
             </p>
+
+            <p>
             It's time to get together to discuss, collaborate, and celebrate Ravencoin.
+            </p>
+
             <p>
             <strong>Where:</strong> Aboard the Carnival Horizon</p>
             <strong>When:</strong> Nov 13, 2021 - Nov 21, 2021
             <strong>Who:</strong> The Ravencoin community -- that's you.
             <a href='https://www.carnival.com/booking/review?currency=USD&durDays=8&embkCode=MIA&isMilitary=N&isOver55=N&isPastGuest=N&itinCode=SCW&locality=1&numGuests=2&qbMetaCode=IS&qbPrice=654&qbRateCode=PSV&sailDate=11132021&sailingID=91434&shipCode=HZ&subRegionCode=CS&country=US'>[Book Now]</a>
             </p>
+
             <p>
             These massive cruise ships have <a href='https://www.carnival.com/internet-plans'>internet available</a> and are perfect for remote work, or just relax and join the on-board activities.  If our event is large enough, we can get <a href='https://www.carnivalmeetings.com/plan/corporate-events/'>additional support</a> from the cruise line.
             </p>
+
             <p>
             <a href='https://www.carnival.com/booking/review?currency=USD&durDays=8&embkCode=MIA&isMilitary=N&isOver55=N&isPastGuest=N&itinCode=SCW&locality=1&numGuests=2&qbMetaCode=IS&qbPrice=654&qbRateCode=PSV&sailDate=11132021&sailingID=91434&shipCode=HZ&subRegionCode=CS&country=US'><img src={meetupimg}><a>
             </p>
+
         </Container>
       </StyledMeetup>
     </Layout>
