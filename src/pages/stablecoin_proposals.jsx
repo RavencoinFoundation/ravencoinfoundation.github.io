b272bb4acc04d35717af81b04fed1fecd762416c
Tron <tron@Trons-MacBook-Pro.local>
2021-09-02 14:52:00 -0600

    Add stablecoin project contribution page

--- src/pages/stablecoin_proposals.jsx
+++ src/pages/stablecoin_proposals.jsx
@@ -12,6 +12,7 @@ const StyledProposals = styled.div`
   text-align: left;
   min-height: 50vh;
   font-size: 3vh;
+  .sub {font-size: 2vh;}
   @media (min-width: ${theme.screens.tablet}) {
   }
   @media (min-width: ${theme.screens.laptop}) {
@@ -40,9 +41,9 @@ function Proposals() {
             <h3>OpenDAO - USDO</h3>
             <p>This proposal is ready to go.  It requires $25,000 worth of RVN for the OpenDAO project to incentivize people to use pRVN as collateral to back USDO which uses an algorithmically stablized smart contract on the Binance Smart Chain.  This will create a USDO token backed by pRVN.</p>
             <p>This project will allow borrowing a USDO stablecoin from your RVN.</p>
-            <p>Required: $25,000 in RVN.</p>
-            <p>Details: OpenDAO Proposal (soon)</p>
-            <p>RVN Contribution Address: RJzkp2xcXkEQYXYfZzdBTrvCNxy2GKqjLn</p>
+            <p>Required: $50,000 in RVN.</p>
+            <p>Details: <a href='https://medium.com/opendao/opendao-x-ocp-integration-how-and-why-newly-accepted-usdo-stablecoin-collateral-defi-farms-3e7ddc8bd7be'>OpenDAO Proposal</a></p>
+            <p>RVN Contribution Address<sup>*</sup>: RJzkp2xcXkEQYXYfZzdBTrvCNxy2GKqjLn</p>
             </p>
 
             <br></br>
@@ -52,9 +53,11 @@ function Proposals() {
             <p>This project put a stablecoin directly on the Ravencoin blockchain which can be used in on-chain atomic swaps.</p>
             <p>Required: $125,000.</p>
             <p>Details - <a href='/Stably_RavenCoin_Proposal_v2.pdf'>Stably Proposal as PDF</a></p>
-            <p>RVN Contribution Address: RBzm8wmbEcdFxAWAZ2stkxSN615uDgvqCd</p>
+            <p>RVN Contribution Address<sup>*</sup>: RBzm8wmbEcdFxAWAZ2stkxSN615uDgvqCd</p>
             </p>
  
+            <br></br>
+            <div className="sub"><sup>*</sup>Contributions over $1000 will be returned to the originating address if the project does not reach the funding goal. Smaller contributions will be combined with Ravencoin Foundation general fund. Do not send returnable contributions from a custodial service. To prevent a larger contribution from being returned, send [X].1234 qty.</div>   
         </Container>
       </StyledProposals>
     </Layout>
